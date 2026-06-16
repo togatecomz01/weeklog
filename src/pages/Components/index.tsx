ff7ad8a79f4455062a791fdac6eac04e660ba10c
minjunggggg <togatecomz.mj@gmail.com>
2026-06-16 15:52:13 +0900

    컴포넌트 예시 페이지

---
+++ src/pages/Components/index.tsx
@@ -0,0 +1,41 @@
+import ButtonContainer from '@/components/ButtonContainer'
+import './Components.scss'
+import Button from '@/components/Button'
+
+function Components() {
+    return (
+        <div>
+            <h1>공통</h1>
+            <h2>하단 2단 버튼</h2>
+            <div className='section'>
+                <h3>기본</h3>
+                <ButtonContainer>
+                    <Button variant="secondary">임시저장</Button>
+                    <Button>등록</Button>
+                </ButtonContainer>
+            </div>
+
+            <div className='section'>
+                <h3>disabled</h3>
+                <ButtonContainer>
+                    <Button variant="secondary" disabled>임시저장</Button>
+                    <Button disabled>등록</Button>
+                </ButtonContainer>
+            </div>
+
+            <h2>full 버튼</h2>
+            <div className='section'>
+                <Button variant="full">더보기</Button>
+                <Button variant="full">로그인</Button>
+                <Button variant="full">스윗으로 보내기</Button>
+                <Button variant="full">전송 완료</Button>
+            </div>
+
+            <h2>기타</h2>
+            <h2>뱃지</h2>
+
+        </div>
+    )
+}
+
+export default Components
