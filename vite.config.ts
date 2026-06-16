532ef9883d6268cbf7e3b1be91861d28a4f39849
minjunggggg <togatecomz.mj@gmail.com>
2026-06-16 10:47:10 +0900

    패키지 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/weeklog/',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '${path.resolve(__dirname, 'src/styles/variables')}' as *;`,
+      },
+    },
+  },
 })
