d0b01f3eaf1e2919e585428092dfc558d316a565
leesoli0122 <leesoli0122@gmail.com>
2026-06-17 14:16:26 +0900

    fill, stroke 색상 제거, "currentColor"로 변경

--- src/components/icons/SendIcon.tsx
+++ src/components/icons/SendIcon.tsx
@@ -1,8 +1,8 @@
 function SendIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-      <path d="M15.7501 0.75L7.41675 9.0833" stroke="#454545" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M15.75 0.75L10.75 15.75L7.41667 9.0833L0.75 5.75L15.75 0.75Z" stroke="#454545" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M15.7501 0.75L7.41675 9.0833" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M15.75 0.75L10.75 15.75L7.41667 9.0833L0.75 5.75L15.75 0.75Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
