d486da9d8b5e51f3aadf1a78eb7db39a222ea979
leesoli0122 <leesoli0122@gmail.com>
2026-06-17 15:09:07 +0900

    1차 버튼 수정 완

--- src/components/icons/CheckCircleIcon.tsx
+++ src/components/icons/CheckCircleIcon.tsx
@@ -1,6 +1,6 @@
 function CheckCircleIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
       <path d="M8.25 15.75C12.3921 15.75 15.75 12.3921 15.75 8.25C15.75 4.10786 12.3921 0.75 8.25 0.75C4.10786 0.75 0.75 4.10786 0.75 8.25C0.75 12.3921 4.10786 15.75 8.25 15.75Z" stroke="currentColor" strokeWidth="1.5"/>
       <path d="M4.75 8.25L7.08333 10.75L11.75 5.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
