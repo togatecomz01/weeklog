86a46dd53dc327da6337e6e0c230a157379db26e
minjunggggg <togatecomz.mj@gmail.com>
2026-06-17 15:48:05 +0900

    input label 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,16 +2,24 @@ import './Input.scss'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string
+  label?: string
+  required?: boolean
   error?: boolean
   errorMessage?: string
 }
 
-function Input({ className = '', error = false, errorMessage, ...props }: InputProps) {
+function Input({ className = '', label, required = false, error = false, errorMessage, id, ...props }: InputProps) {
   const inputClasses = ['input', error ? 'input--error' : '', className].filter(Boolean).join(' ')
 
   return (
     <div className="input-wrapper">
-      <input className={inputClasses} {...props} />
+      {label && (
+        <label className="input-wrapper__label" htmlFor={id}>
+          {label}
+          {required && <span className="input-wrapper__required" aria-hidden>*</span>}
+        </label>
+      )}
+      <input className={inputClasses} id={id} required={required} {...props} />
       {error && errorMessage && (
         <p className="input-wrapper__error">{errorMessage}</p>
       )}
