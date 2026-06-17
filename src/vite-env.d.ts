b6c756bdd8fe6a632bc86a83aca380785d3bdeda
minjunggggg <togatecomz.mj@gmail.com>
2026-06-17 10:30:12 +0900

    이미지 경로 생성, 로고 업로드

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,18 @@ declare module '*.scss' {
   const content: Record<string, string>
   export default content
 }
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
