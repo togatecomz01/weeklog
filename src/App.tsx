c798f64874f5a3d62f1dd3fc366650f8c28708f0
minjunggggg <togatecomz.mj@gmail.com>
2026-06-18 17:30:38 +0900

    마이페이지 초안

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './pages/Main'
+import MyPage from './pages/MyPage'
 import Components from './pages/Components'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/weeklog/login" element={<Login />} />
         <Route path="/weeklog/main" element={<Main />} />
+        <Route path="/weeklog/my" element={<MyPage />} />
         <Route path="/weeklog/components" element={<Components />} />
       </Routes>
     </BrowserRouter>
