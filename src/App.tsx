dbacafb5e2620a43d90726f7a4b4d248ca157d7a
minjunggggg <togatecomz.mj@gmail.com>
2026-06-19 15:58:26 +0900

    비밀번호 초기화 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './pages/Main'
 import MyPage from './pages/MyPage'
+import ResetPassword from './pages/ResetPassword'
 import Entry from './pages/Entry'
 import Components from './pages/Components'
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/weeklog/login" element={<Login />} />
         <Route path="/weeklog/main" element={<Main />} />
         <Route path="/weeklog/my" element={<MyPage />} />
+        <Route path="/weeklog/reset-password" element={<ResetPassword />} />
         <Route path="/weeklog/entry" element={<Entry />} />
         <Route path="/weeklog/components" element={<Components />} />
       </Routes>
