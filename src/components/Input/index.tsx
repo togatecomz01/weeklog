e3f026afdddbb8dbeb57f69411fbc6963c4c12dd
minjunggggg <togatecomz.mj@gmail.com>
2026-06-19 14:26:14 +0900

    인풋 힌트 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,11 +4,12 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string
   label?: string
   required?: boolean
+  hint?: string
   error?: boolean
   errorMessage?: string
 }
 
-function Input({ className = '', label, required = false, error = false, errorMessage, id, ...props }: InputProps) {
+function Input({ className = '', label, required = false, hint, error = false, errorMessage, id, ...props }: InputProps) {
   const inputClasses = ['input', error ? 'error' : '', className].filter(Boolean).join(' ')
 
   return (
@@ -20,6 +21,7 @@ function Input({ className = '', label, required = false, error = false, errorMe
         </label>
       )}
       <input className={inputClasses} id={id} required={required} {...props} />
+      {hint && !error && <p className="input-hint">{hint}</p>}
       {error && errorMessage && (
         <p className="input-error">{errorMessage}</p>
       )}
