27ada0c4747be869741bf3e83a0f51efa94afd49
leesoli0122 <leesoli0122@gmail.com>
2026-06-26 15:14:57 +0900

    header 색상 두가지로 변경

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './AppHeader.scss'
 
+type HeaderVariant = '' | 'basics'
 interface AppHeaderProps {
   left?: React.ReactNode
   right?: React.ReactNode
+  variant?: HeaderVariant
 }
 
-function AppHeader({ left, right }: AppHeaderProps) {
+function AppHeader({ left, right, variant = '' }: AppHeaderProps) {
   return (
-    <header className="app-header">
+    <header className={`app-header ${variant}`}>
       <div className="app-header-left">{left}</div>
       {right && <div className="app-header-right">{right}</div>}
     </header>
