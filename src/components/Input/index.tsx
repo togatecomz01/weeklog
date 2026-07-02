8eaf54ab4706ab0bc1afac8a8ed6a36487589398
minjunggggg <togatecomz.mj@gmail.com>
2026-07-02 13:48:54 +0900

    인풋 클릭 이벤트 수정

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import './Input.scss'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -9,18 +10,30 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
 }
 
-function Input({ className = '', label, required = false, hint, error = false, errorMessage, id, ...props }: InputProps) {
+function Input({ className = '', label, required = false, hint, error = false, errorMessage, id, onClick, ...props }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
   const inputClasses = ['input', error ? 'error' : '', className].filter(Boolean).join(' ')
+
+  function handleClick(e: React.MouseEvent<HTMLInputElement>) {
+    if (props.type === 'date') {
+      (inputRef.current as any)?.showPicker?.()
+    }
+    onClick?.(e)
+  }
 
   return (
     <div className="input-group">
       {label && (
-        <label className="input-label" htmlFor={id}>
+        <label
+          className="input-label"
+          htmlFor={id}
+          onClick={(e) => e.preventDefault()}
+        >
           {label}
           {required && <span className="input-required" aria-hidden>*</span>}
         </label>
       )}
-      <input className={inputClasses} id={id} required={required} {...props} />
+      <input ref={inputRef} className={inputClasses} id={id} required={required} onClick={handleClick} {...props} />
       {hint && !error && <p className="input-hint">{hint}</p>}
       {error && errorMessage && (
         <p className="input-error">{errorMessage}</p>
